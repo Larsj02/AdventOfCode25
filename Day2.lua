53437dd2f7505ec67cfc80959fb904fe45769473
Lars Johnen <l.m.johnen@gmail.com>
2025-12-02 10:19:19 +0100

    Day 2 [Part 1 & 2]

--- Day2.lua
+++ Day2.lua
@@ -1,5 +1,7 @@
 ---@class Validator
-local Validator = {}
+local Validator = {
+    _anyRepeats = false,
+}
 
 ---@param numberStr string|number
 ---@return boolean isValid
@@ -8,21 +10,42 @@ function Validator:Validate(numberStr)
     if numberStr[1] == "0" then
         return false
     end
-    local nLen = #numberStr
-    if nLen % 2 ~= 0 then
+
+    if self._anyRepeats then
+        local nLen = #numberStr
+        for i = nLen - 1, 1, -1 do
+            if nLen % i == 0 then
+                local lastSegment
+                local fullMatch = true
+                for s = 1, nLen, i do
+                    local segment = numberStr:sub(s, s + i - 1)
+                    if not lastSegment then lastSegment = segment end
+                    if segment ~= lastSegment then
+                        fullMatch = false
+                        break
+                    end
+                end
+                if fullMatch then return false end
+            end
+        end
         return true
+    else
+        local nLen = #numberStr
+        if nLen % 2 ~= 0 then
+            return true
+        end
+        local first = numberStr:sub(1, nLen / 2)
+        local last = numberStr:sub(nLen / 2 + 1, nLen)
+        return first ~= last
     end
-    local first = numberStr:sub(1, nLen / 2)
-    local last = numberStr:sub(nLen / 2 + 1, nLen)
-    return first ~= last
 end
 
----@param firstNum string|number
----@param lastNum string|number
+---@param firstNum string|number|nil
+---@param lastNum string|number|nil
 ---@return number sum
 function Validator:ValidateRange(firstNum, lastNum)
     local sum = 0
-    local firstNum, lastNum = tonumber(firstNum), tonumber(lastNum)
+    firstNum, lastNum = tonumber(firstNum), tonumber(lastNum)
     if not (firstNum and lastNum) then
         return 0
     end
@@ -44,16 +67,22 @@ function Validator:ValidateSequence(sequence)
     return sum
 end
 
+---@param anyRepeats boolean|nil
 ---@return Validator
-function Validator:New()
+function Validator:New(anyRepeats)
     local val = setmetatable({}, {
         __index = Validator
     })
 
+    if anyRepeats then
+        val._anyRepeats = true
+    end
+
     return val
 end
 
-local val = Validator:New()
+local val = Validator:New(true)
+
 print(val:ValidateSequence([[
   9595822750-9596086139,1957-2424,88663-137581,48152-65638,12354817-12385558,435647-489419,518494-609540,2459-3699,646671-688518,195-245,295420-352048,346-514,8686839668-8686892985,51798991-51835611,8766267-8977105,2-17,967351-995831,6184891-6331321,6161577722-6161678622,912862710-913019953,6550936-6625232,4767634976-4767662856,2122995-2257010,1194-1754,779-1160,22-38,4961-6948,39-53,102-120,169741-245433,92902394-92956787,531-721,64-101,15596-20965,774184-943987,8395-11781,30178-47948,94338815-94398813
 ]]))
