d180b2ee010d1d04d6ab6b8236cb956fa34ca32c
Lars <52138902+Larsj02@users.noreply.github.com>
2025-12-03 14:27:47 +0000

    Day 3 [Part 1 & 2]

--- Day3.lua
+++ Day3.lua
@@ -1,68 +1,75 @@
 ---@class Row
 local Row = {
-  ---@type number[]
-  _nums = {},
+    ---@type number[]
+    _nums = {}
 }
 
 ---@param nums string
 ---@return Row row
 function Row:New(nums)
-  local row = setmetatable({}, {__index = Row})
+    local row = setmetatable({}, {__index = Row})
 
-  local numTbl = {}
-  for num in nums:gmatch("%d") do
-    table.insert(numTbl, tonumber(num))
-  end
-  row._nums = numTbl
+    local numTbl = {}
+    for num in nums:gmatch("%d") do table.insert(numTbl, tonumber(num)) end
+    row._nums = numTbl
 
-  return row
+    return row
 end
 
+---@param batteryCount number
 ---@return number maxJolt
-function Row:GetMaxJolt()
-  local sum = 0
-  local high, high2 = 0, 0
-  for i, num in ipairs(self._nums) do
-    if num > high and i < #self._nums then
-      high = num
-      high2 = 0
-    elseif num > high2 then
-        high2 = num
-    end
-  end
+function Row:GetMaxJolt(batteryCount)
+    local high, lastI = "", 1
 
-  return tonumber(high..high2)
+    for i = 1, batteryCount do
+        local maxNum, maxI = -1, -1
+        for numI = lastI, #self._nums - (batteryCount - i) do
+            local num = self._nums[numI]
+            if num > maxNum then
+                maxNum = num
+                maxI = numI
+            end
+        end
+
+        high = high .. maxNum
+        lastI = maxI + 1
+    end
+
+    return tonumber(high)
 end
 
 ---@class Bank
 local Bank = {
-  ---@type Row[]
-  _rows = {}
+    ---@type Row[]
+    _rows = {},
+    ---@type number
+    _batteryCount = 2
 }
 
 ---@param rowsStr string
+---@param batteryCount number
 ---@return Bank bank
-function Bank:New(rowsStr)
-  local bank = setmetatable({}, {__index = Bank})
+function Bank:New(rowsStr, batteryCount)
+    local bank = setmetatable({}, {__index = Bank})
 
-  local rows = {}
-  for rowStr in rowsStr:gmatch("[^\n]+") do
-    local row = Row:New(rowStr)
-    table.insert(rows, row)
-  end
-  bank._rows = rows
-
-  return bank
+    local rows = {}
+    for rowStr in rowsStr:gmatch("[^\n]+") do
+        local row = Row:New(rowStr)
+        table.insert(rows, row)
+    end
+    bank._rows = rows
+    bank._batteryCount = batteryCount
+    return bank
 end
 
 ---@return number totalJolt
 function Bank:GetTotalJolt()
-  local sum = 0
-  for _, row in ipairs(self._rows) do
-    sum = sum + row:GetMaxJolt()
-  end
+    local sum = 0
+    for _, row in ipairs(self._rows) do
+        sum = sum + row:GetMaxJolt(self._batteryCount)
+    end
 
-  return sum
+    return sum
 end
 
 local bank = Bank:New([[
@@ -266,6 +273,6 @@ local bank = Bank:New([[
 5311474353345337334323253357343334153332313242123235333533247333223223444223272363533336621352323364
 2113321622353266323322337212258272559612637563223212222122323524122223231223433433223251542744472224
 2222414441224345244433322212544342444333132413533622441534143243232433242625254243433385231332255231
-  ]])
+]], 12)
 
 print(bank:GetTotalJolt())
