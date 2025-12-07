167e699744eb66a19512f4dddacb6b7a197bd6b0
Lars <l.m.johnen@gmail.com>
2025-12-07 21:44:55 +0100

    Day 6 Simple Part 1

--- Day6.lua
+++ Day6.lua
@@ -1,1 +1,39 @@
 local input = require("Input")
+
+local arr = {}
+local operations = {}
+local rowCount = 1
+for row in input:gmatch("[^\n]+") do
+    local colCount = 1
+    for col in row:gmatch("[^%s]+") do
+        if not arr[colCount] then
+            arr[colCount] = {}
+        end
+        local num = tonumber(col)
+        if not num then
+            operations[colCount] = col
+        else
+            arr[colCount][rowCount] = num
+        end
+        colCount = colCount + 1
+    end
+    rowCount = rowCount + 1
+end
+
+local totalSum = 0
+for colNum, operation in pairs(operations) do
+    if operation == "+" then
+        local sum = 0
+        for rowNum = 1, #arr[colNum] do
+            sum = sum + arr[colNum][rowNum]
+        end
+        totalSum = totalSum + sum
+    elseif operation == "*" then
+        local sum = 1
+        for rowNum = 1, #arr[colNum] do
+            sum = sum * arr[colNum][rowNum]
+        end
+        totalSum = totalSum + sum
+    end
+end
+print("Total Sum: " .. totalSum)
