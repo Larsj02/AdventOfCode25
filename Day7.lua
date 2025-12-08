6c3078eecbbf7138d423c437436226f47141bcbf
Lars <52138902+Larsj02@users.noreply.github.com>
2025-12-08 10:01:25 +0000

    Day 7 Simple Part 1

--- Day7.lua
+++ Day7.lua
@@ -1,1 +1,38 @@
 local input = require("Input")
+
+local grid = {}
+
+local row = 1
+for rowStr in input:gmatch("[^\n]+") do
+    grid[row] = {}
+    local col = 1
+    for char in rowStr:gmatch(".") do
+        grid[row][col] = char
+        col = col + 1
+    end
+    row = row + 1
+end
+
+local splitCount = 0
+for y, xTbl in ipairs(grid) do
+    for x, char in pairs(xTbl) do
+      if char == "S" then
+        grid[y+1][x] = "|"
+      elseif char == "^" and grid[y-1][x] == "|" then
+        splitCount = splitCount + 1
+        grid[y][x-1] = "|"
+        grid[y][x+1] = "|"
+      elseif grid[y-1] and grid[y-1][x] == "|" then
+        grid[y][x] = "|"
+      end
+    end
+end
+
+for x, xTbl in ipairs(grid) do
+    local line = ""
+    for y, char in pairs(xTbl) do
+        line = line .. char
+    end
+    print(line)
+end
+print(("Solution 1: %d"):format(splitCount))
