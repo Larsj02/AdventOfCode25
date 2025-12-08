9e4aac89262715a7339d91158e31358dc4b6d85e
Lars <52138902+Larsj02@users.noreply.github.com>
2025-12-08 10:49:51 +0000

    Day 7 [Part 1]

--- Day7.lua
+++ Day7.lua
@@ -1,38 +1,98 @@
 local input = require("Input")
 
-local grid = {}
+---@enum CellType
+local CellType = {Start = "S", Splitter = "^", Path = "|", Empty = "."}
 
-local row = 1
-for rowStr in input:gmatch("[^\n]+") do
-    grid[row] = {}
-    local col = 1
-    for char in rowStr:gmatch(".") do
-        grid[row][col] = char
-        col = col + 1
-    end
-    row = row + 1
+---@class Cell
+local Cell = {
+    ---@type number
+    _x = 0,
+    ---@type number
+    _y = 0,
+    ---@type CellType
+    _type = ""
+}
+
+---@param x number
+---@param y number
+---@param cellType CellType
+---@return Cell cell
+function Cell:New(x, y, cellType)
+    local cell = setmetatable({}, {__index = Cell})
+    cell._x = x
+    cell._y = y
+    cell._type = cellType
+    return cell
 end
 
-local splitCount = 0
-for y, xTbl in ipairs(grid) do
-    for x, char in pairs(xTbl) do
-      if char == "S" then
-        grid[y+1][x] = "|"
-      elseif char == "^" and grid[y-1][x] == "|" then
-        splitCount = splitCount + 1
-        grid[y][x-1] = "|"
-        grid[y][x+1] = "|"
-      elseif grid[y-1] and grid[y-1][x] == "|" then
-        grid[y][x] = "|"
-      end
+---@class Grid
+local Grid = {
+    ---@type Cell[][]
+    _cells = {}
+}
+
+function Grid:New()
+    local grid = setmetatable({}, {__index = Grid})
+    return grid
+end
+
+---@param input string
+function Grid:ParseInput(input)
+    self._cells = {}
+    local row = 1
+    for rowStr in input:gmatch("[^\n]+") do
+        self._cells[row] = {}
+        local col = 1
+        for char in rowStr:gmatch(".") do
+            self._cells[row][col] = Cell:New(col, row, char)
+            col = col + 1
+        end
+        row = row + 1
     end
 end
 
-for x, xTbl in ipairs(grid) do
-    local line = ""
-    for y, char in pairs(xTbl) do
-        line = line .. char
+function Grid:PrintGrid(grid)
+    for y, xTbl in ipairs(grid or self._cells) do
+        local line = ""
+        for x, cell in pairs(xTbl) do line = line .. cell._type end
+        print(line)
     end
-    print(line)
 end
+
+function Grid:GetCopy(tbl)
+    local newTbl = {}
+    for k, v in pairs(tbl) do
+        if type(v) == "table" then
+            newTbl[k] = self:GetCopy(v)
+        else
+            newTbl[k] = v
+        end
+    end
+    return newTbl
+end
+
+---@return number splitCount
+---@return Cell[][] solutionGrid
+function Grid:GetSolution1()
+    local grid = self:GetCopy(self._cells)
+    local splitCount = 0
+    for y, xTbl in ipairs(grid) do
+        for x, cell in pairs(xTbl) do
+            if cell._type == "S" then
+                grid[y + 1][x] = Cell:New(x, y + 1, "|")
+            elseif cell._type == "^" and grid[y - 1] and grid[y - 1][x]._type == "|" then
+                splitCount = splitCount + 1
+                grid[y][x - 1] = Cell:New(x - 1, y, "|")
+                grid[y][x + 1] = Cell:New(x + 1, y, "|")
+            elseif grid[y - 1] and grid[y - 1][x]._type == "|" then
+                grid[y][x] = Cell:New(x, y, "|")
+            end
+        end
+    end
+    return splitCount, grid
+end
+
+local grid = Grid:New()
+grid:ParseInput(input)
+local splitCount, solutionGrid = grid:GetSolution1()
 print(("Solution 1: %d"):format(splitCount))
