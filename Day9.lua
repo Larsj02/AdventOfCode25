04791bf21df40398eafb2fb9355a8bd6461af9b0
Lars <l.m.johnen@gmail.com>
2025-12-09 21:42:52 +0100

    Day 9 [Part 1]

--- Day9.lua
+++ Day9.lua
@@ -1,42 +1,88 @@
 local input = require("Input")
 
-local coords = {}
+---@enum COLOR
+local COLOR = {
+    RED = 1,
+    GREEN = 2,
+    NONE = 3
+}
 
----@class Coordinate
----@field x number
----@field y number
+---@class Tile
+local Tile = {
+    ---@type number
+    _x = 0,
+    ---@type number
+    _y = 0,
+    ---@type COLOR
+    _color = COLOR.NONE
+}
 
----@param coord1 Coordinate
----@param coord2 Coordinate
+---@param x number
+---@param y number
+---@param color COLOR|nil
+---@return Tile tile
+function Tile:New(x, y, color)
+    local tile = setmetatable({}, { __index = self })
+    tile._x = x
+    tile._y = y
+    tile._color = color or COLOR.NONE
+    return tile
+end
+
+---@class TileList
+local TileList = {
+    ---@type Tile[]
+    _tiles = {}
+}
+
+---@return TileList tileList
+function TileList:New()
+    local tileList = setmetatable({}, { __index = self })
+    tileList._tiles = {}
+    return tileList
+end
+
+---@param tile Tile
+function TileList:AddTile(tile)
+    table.insert(self._tiles, tile)
+end
+
+---@param str string
+function TileList:ParseList(str)
+    for x, y in str:gmatch("(%d+),(%d+)\n*") do
+        self:AddTile(Tile:New(tonumber(x), tonumber(y), COLOR.RED))
+    end
+end
+
+---@param tileA Tile
+---@param tileB Tile
 ---@return number area
-local function getSquare(coord1, coord2)
-    local width = math.abs(coord1.x - coord2.x) + 1
-    local height = math.abs(coord1.y - coord2.y) + 1
+function TileList:GetSquareArea(tileA, tileB)
+    local width = math.abs(tileA._x - tileB._x) + 1
+    local height = math.abs(tileA._y - tileB._y) + 1
     return width * height
 end
 
-local function getBiggestSquare()
+---@return number area, {tileA: Tile, tileB: Tile} biggestTiles
+function TileList:GetBiggestSquare()
     local biggestArea = 0
-    local biggestCoords = nil
-    for i = 1, #coords do
-        for j = i + 1, #coords do
-            local area = getSquare(coords[i], coords[j])
-            if area > biggestArea then
-                biggestArea = area
-                biggestCoords = { coords[i], coords[j] }
+    ---@type {tileA: Tile, tileB: Tile}
+    local biggestTiles = {}
+    for _, tileA in ipairs(self._tiles) do
+        for _, tileB in ipairs(self._tiles) do
+            if tileA ~= tileB then
+                local area = self:GetSquareArea(tileA, tileB)
+                if area > biggestArea then
+                    biggestArea = area
+                    biggestTiles = { tileA = tileA, tileB = tileB }
+                end
             end
         end
     end
-    return biggestArea, biggestCoords
+    return biggestArea, biggestTiles
 end
 
-for x, y in input:gmatch("(%d+),(%d+)\n*") do
-    table.insert(coords, { x = tonumber(x), y = tonumber(y) })
-end
-
-local area, biggestCoords = getBiggestSquare()
-print("Biggest area:", area)
-print("Coordinates:")
-for _, coord in ipairs(biggestCoords) do
-    print(string.format("(%d, %d)", coord.x, coord.y))
-end
+local tileList = TileList:New()
+tileList:ParseList(input)
+local area = tileList:GetBiggestSquare()
+print(("Solution 1: %d"):format(area))
